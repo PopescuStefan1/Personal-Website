47b5a38ae80acf1fb27c6b61f8b648e01fed93e3
PopescuStefan1 <stefan.ro95@gmail.com>
2025-02-18 21:35:46 +0200

    Add animated background

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,52 @@ import { MdComputer } from "react-icons/md";
 function App() {
     return (
         <>
-            <div className="container">
-                <div className="header-div">
-                    <div className="header-div-cell centered-cell">
-                        <div className="icon-div">
-                            <div className="grow-div"></div>
-                            <IconContext.Provider value={{ size: "2em" }}>
-                                <div className="icon-container">
-                                    <MdComputer />
-                                </div>
-                            </IconContext.Provider>
-                            <div className="icon-vertical-line"></div>
+            <div className="context">
+                <div className="container">
+                    <div className="header-div">
+                        <div className="header-div-cell centered-cell">
+                            <div className="icon-div">
+                                <div className="grow-div"></div>
+                                <IconContext.Provider value={{ size: "2em" }}>
+                                    <div className="icon-container">
+                                        <MdComputer />
+                                    </div>
+                                </IconContext.Provider>
+                                <div className="icon-vertical-line"></div>
+                            </div>
+                        </div>
+                        <div className="header-div-cell">
+                            <h2>
+                                Fullstack <span className="accent">Developer</span>, Cybersecurity Student & Tech{" "}
+                                <span className="accent">Enthusiast</span>
+                            </h2>
+                        </div>
+                        <div className="header-div-cell centered-cell">
+                            <div className="vertical-line"></div>
+                        </div>
+                        <div className="header-div-cell">
+                            <h3>
+                                Clean code, scalable systems, and a whole lot of passion, that&apos;s how I aspire to
+                                engineer solutions.
+                            </h3>
                         </div>
                     </div>
-                    <div className="header-div-cell">
-                        <h2>
-                            Fullstack <span className="accent">Developer</span>, Cybersecurity Student & Tech{" "}
-                            <span className="accent">Enthusiast</span>
-                        </h2>
-                    </div>
-                    <div className="header-div-cell centered-cell">
-                        <div className="vertical-line"></div>
-                    </div>
-                    <div className="header-div-cell">
-                        <h3>
-                            Clean code, scalable systems, and a whole lot of passion, that&apos;s how I engineer
-                            solutions.
-                        </h3>
-                    </div>
                 </div>
+            </div>
+
+            <div className="area">
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         </>
     );
