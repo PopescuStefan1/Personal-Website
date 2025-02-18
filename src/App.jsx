9e924a6c352243709e1ad3abb985afabc2c2dfb3
PopescuStefan1 <stefan.ro95@gmail.com>
2025-02-18 21:13:17 +0200

    Improve header styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,30 @@ function App() {
             <div className="container">
                 <div className="header-div">
                     <div className="header-div-cell centered-cell">
-                        <IconContext.Provider value={{ size: "2em" }}>
-                            <div className="icon-container">
-                                <MdComputer />
-                            </div>
-                        </IconContext.Provider>
+                        <div className="icon-div">
+                            <div className="grow-div"></div>
+                            <IconContext.Provider value={{ size: "2em" }}>
+                                <div className="icon-container">
+                                    <MdComputer />
+                                </div>
+                            </IconContext.Provider>
+                            <div className="icon-vertical-line"></div>
+                        </div>
                     </div>
                     <div className="header-div-cell">
-                        <h2>Fullstack Developer, Cybersecurity Student & Tech Enthusiast</h2>
+                        <h2>
+                            Fullstack <span className="accent">Developer</span>, Cybersecurity Student & Tech{" "}
+                            <span className="accent">Enthusiast</span>
+                        </h2>
                     </div>
                     <div className="header-div-cell centered-cell">
                         <div className="vertical-line"></div>
                     </div>
                     <div className="header-div-cell">
-                        <h3>I craft clean, scalable code solutions and I love what I do.</h3>
+                        <h3>
+                            Clean code, scalable systems, and a whole lot of passion, that&apos;s how I engineer
+                            solutions.
+                        </h3>
                     </div>
                 </div>
             </div>
